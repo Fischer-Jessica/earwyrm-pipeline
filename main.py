e0e7dc17e2378758dffbde905987e75bb3bed3cf
fischer <Fischer.Jessica.HTL@gmail.com>
2025-06-21 11:46:56 +0200

    Add language selection support for TTS models

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import os
 import pvorca
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-access_key = os.getenv("ACCESS_KEY")
-output_path = os.getenv("OUTPUT_PATH")
+language = "en"
+text = "This is a test to find out how the Orca TTS sounds in different languages."
 
-text = "This is a test to find out how the Orca TTS sounds and how it works."
+if language == "de":
+    model_path = os.getenv("MODEL_PATH") + "orca_params_de_male.pv"
+else:
+    model_path = os.getenv("MODEL_PATH") + "orca_params_en_male.pv"
 
-orca = pvorca.create(access_key=access_key)
+orca = pvorca.create(access_key=os.getenv("ACCESS_KEY"), model_path=model_path)
 
-orca.synthesize_to_file(text=text, output_path=output_path)
+orca.synthesize_to_file(text=text, output_path=os.getenv("OUTPUT_PATH"))
 
 orca.delete()
